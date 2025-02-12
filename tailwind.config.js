b64ceb1b42d1d451abef4ab2ffb8ac6488af4efc
NaimaBelchiti300 <naimabelchiti07@gmail.com>
2025-02-12 10:41:34 +0100

    'addstyle'

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Add the paths to your React files
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+        lora: ['Lora', 'serif'],
+      },
+      colors: {
+        verte: '#8CA658',
+        gray: '#424242',
+      },
+    },
+  },
+  plugins: [],
+}
