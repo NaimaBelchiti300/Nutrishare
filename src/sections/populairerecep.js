900f01312733ee1e2c4c1a0c8aab4fd1c98655f9
NaimaBelchiti300 <naimabelchiti07@gmail.com>
2025-02-14 10:38:53 +0100

    'added-caroucelle-sectio'

--- src/sections/populairerecep.js
+++ src/sections/populairerecep.js
@@ -10,12 +10,43 @@ import r5 from "../images/recette5.PNG";
 
 const Populairerecepe = () => {
   const cards = [
-    { image: r1, title: "Vegan Super Bowl", description: "Un bol rempli de fraîcheur et de couleurs ! Ce plat vegan combine des légumes croquants, des herbes aromatiques et une touche de citron vert pour une explosion de saveurs saines et délicieuses",button1:'btn1',button2:'btn2' },
-    { image: r4, title: "Crêpes Gourmandes à la Chandeleur", description: "Des crêpes moelleuses nappées de sirop et garnies de framboises juteuses. Parfaites pour un petit-déjeuner gourmand ou une pause sucrée réconfortante. ",  button1:'btn1',button2:'btn2' },
-    { image: r5, title: "Poulet au Lait de Coco", description: " Découvrez cette recette exotique de poulet cuit dans une sauce onctueuse au lait de coco. Avec des champignons et des épices, ce plat est riche en saveurs et parfait pour un dîner réconfortant. ",  button1:'btn1',button2:'btn2' },
-    { image: r3, title: "Crêpes en Folie pour la Chandeleur", description: "Profitez de la Chandeleur avec ces délicieuses crêpes sucrées au miel,  à la fois légères et moelleuses, sont parfaites pour un brunch gourmand ",  button1:'btn1',button2:'btn2' },
-    { image: r2, title: " Tartiflette Fondante au Reblochon", description: "Un plat montagnard généreux et réconfortant, composé de pommes de terre fondantes, de lardons croustillants et d’un reblochon gratiné à la perfection." ,  button1:'btn1',button2:'btn2'},
-  ];
+    { 
+        image: r1, 
+        title: "Vegan Super Bowl", 
+        description: "Un bol rempli de fraîcheur et de couleurs ! Ce plat vegan combine des légumes croquants, des herbes aromatiques et une touche de citron vert pour une explosion de saveurs saines et délicieuses", 
+        button1: 'Temps à préparer : 2h', 
+        button2: 'Calories de la recette : 33kcal' 
+    },
+    { 
+        image: r4, 
+        title: "Crêpes Gourmandes à la Chandeleur", 
+        description: "Des crêpes moelleuses nappées de sirop et garnies de framboises juteuses. Parfaites pour un petit-déjeuner gourmand ou une pause sucrée réconfortante.", 
+        button1: 'Temps à préparer : 1h', 
+        button2: 'Calories de la recette : 250kcal' 
+    },
+    { 
+        image: r5, 
+        title: "Poulet au Lait de Coco", 
+        description: "Découvrez cette recette exotique de poulet cuit dans une sauce onctueuse au lait de coco. Avec des champignons et des épices, ce plat est riche en saveurs et parfait pour un dîner réconfortant.", 
+        button1: 'Temps à préparer : 3h', 
+        button2: 'Calories de la recette : 400kcal' 
+    },
+    { 
+        image: r3, 
+        title: "Crêpes en Folie pour la Chandeleur", 
+        description: "Profitez de la Chandeleur avec ces délicieuses crêpes sucrées au miel, à la fois légères et moelleuses, parfaites pour un brunch gourmand.", 
+        button1: 'Temps à préparer : 2h', 
+        button2: 'Calories de la recette : 150kcal' 
+    },
+    { 
+        image: r2, 
+        title: "Tartiflette Fondante au Reblochon", 
+        description: "Un plat montagnard généreux et réconfortant, composé de pommes de terre fondantes, de lardons croustillants et d’un reblochon gratiné à la perfection.", 
+        button1: 'Temps à préparer : 1h30', 
+        button2: 'Calories de la recette : 600kcal' 
+    },
+];
+
 
   const settings = {
     dots: true, // Afficher les points de navigation
@@ -51,23 +82,28 @@ const Populairerecepe = () => {
         {cards.map((card, index) => (
           <div
             key={index}
-            className="flex justify-center items-center px-20 pt-16"
+            className="flex justify-start items-center px-20 pt-16 w-full"
           >
-            <div className="bg-white rounded-xl shadow-md overflow-hidden w-96">
+            <div className="bg-white rounded-xl shadow-md overflow-hidden w-96 space-x-0">
               <img
                 src={card.image}
                 alt={card.title}
-                className="w-full h-96 object-cover"
+                className="w-full h-80 object-cover"
               />
               <div className="p-4 font-serif font-bold">
                 <h2 className="text-2xl font-bold text-gray">
                   {card.title}
                 </h2>
+                <div className="flex flex-col justify-start items-start space-y-2">
                 <p className="text-gray-600 mt-3">{card.description}</p>
-                <div className="flex flex-row justify-start items-center space-x-3 mt-2">
-                  <button className="border-solid border-2 border-verte bg-verte w-44 text-graylight px-5 py-2 rounded-md whitespace-nowrap" >{card.button1}</button>
-                  <button className="border-solid border-2 border-verte px-5 py-2 w-44 text-gray rounded-md whitespace-nowrap">{card.button2}</button>
+                <div className="flex">
+                  
                 </div>
+                <button className="border-solid border-2 border-verte px-5 py-2 w-auto text-gray rounded-md whitespace-nowrap">{card.button2}</button>
+                <button className="border-solid border-2 border-verte px-5 py-2 w-auto  text-gray rounded-md whitespace-nowrap" >{card.button1}</button>
+               
+                </div>
+               
               </div>
             </div>
           </div>
